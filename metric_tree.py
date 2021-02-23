7fee58852b40ecf3d666788f1703ec46af61fdcc
clement <clement.come@student-cs.fr>
2021-02-23 18:49:32 +0100

    Building metric tree following formal algorithm

--- metric_tree.py
+++ metric_tree.py
@@ -8,30 +8,59 @@ class MetricTree:
         self.root = None
         self.mu = None
         self.left = None
+        self.min_left = None
+        self.max_left = None
         self.right = None
+        self.min_right = None
+        self.max_right = None
         self.size = None
-        self.distances = None
         if points is not None:
             self.build(points)
 
-    def median_mu(self, points: np.ndarray, index: int) -> Tuple:
-        pivot = points[[index]]
-        other_points = np.delete(points, index, 0)
-        distance_to_pivot = cdist(pivot, other_points)[0]
-        sorted_indices = np.argsort(distance_to_pivot)
-        return (
-            np.median(distance_to_pivot),
-            distance_to_pivot[sorted_indices],
-            other_points[sorted_indices],
-        )
+    def median_mu(
+        self, points: np.ndarray, pivot: np.ndarray, distances, subset_size: int
+    ) -> float:
+        subset_idx = np.random.choice(len(points) - 1, size=subset_size, replace=False)
+        # subset_points = points[subset_idx]
+        distances_to_pivot = distances[subset_idx]
+        return np.median(distances_to_pivot)
 
-    def left_right_split(self, mu, distances, points):
+    def left_right_split(
+        self, mu: float, points: np.ndarray, distances: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray, float, float, float, float]:
+        """
+        Splits the points into left and right depending on their distance to root node.
+        Points and distances are ordered by ascending distance to root node.
+
+        Parameters
+        ----------
+        mu : float
+            Threshold value for splitting the points
+        points : np.ndarray
+            Array of points to be splitted
+        distances : np.ndarray
+            Distances of the points to the root node
+
+        Returns
+        -------
+        Tuple[np.ndarray, np.ndarray, float, float, float, float]
+            Corresponds to the left and right subtree points with
+            their min and max distances to the root node
+        """
+        if len(points) == 1:
+            dist_to_pivot = distances[0]
+            return points, None, dist_to_pivot, dist_to_pivot, None, None
         idx = np.searchsorted(distances, mu, side="right")
-        distances_left = distances[:idx]
         points_left = points[:idx]
-        distances_right = distances[idx:]
         points_right = points[idx:]
-        return points_left, points_right, distances_left, distances_right
+        return (
+            points_left,
+            points_right,
+            distances[0],
+            distances[idx - 1],
+            distances[idx],
+            distances[-1],
+        )
 
     def pivot_choice(self, points: np.ndarray) -> int:
         ## For the moment only random choice
@@ -39,24 +68,53 @@ class MetricTree:
         return choice_idx
 
     def __repr__(self) -> str:
-        return f"MetricTree with root {self.root} and size {self.size}"
+        return f"MetricTree: root={self.root}, size={self.size}"
 
-    def build(self, points: np.ndarray, distances: np.ndarray = None, tau: float = 0):
+    def __len__(self) -> int:
+        return self.size
+
+    def build(self, points: np.ndarray, subset_size: int = 10000):
         self.size = len(points)
-        if distances is not None:
-            self.distances = distances
         if len(points) == 0:
             return None
+        if len(points) == 1:
+            self.root = points[0]
+            return self
+        ## Selection of the pivot
         pivot_idx = self.pivot_choice(points)
+        remaining_points = np.delete(points, pivot_idx, 0)
         self.root = points[pivot_idx]
-        mu, distances, other_points = self.median_mu(points, pivot_idx)
+
+        ## Compute distances
+        distances = cdist(self.root.reshape(1, *self.root.shape), remaining_points)[0]
+
+        ## Sort the points by distance to the root
+        sorted_indices = np.argsort(distances)
+        distances = distances[sorted_indices]
+        remaining_points = remaining_points[sorted_indices]
+
+        ## Compute median on a subset of remaining points
+        mu = self.median_mu(
+            remaining_points,
+            self.root,
+            distances,
+            min(len(remaining_points) - 1, subset_size),
+        )
         self.mu = mu
+
+        ## Split the points into left and right trees
         (
             points_left,
             points_right,
-            distances_left,
-            distances_right,
-        ) = self.left_right_split(mu, distances, other_points)
-        self.left = MetricTree().build(points_left, distances_left)
-        self.right = MetricTree().build(points_right, distances_right)
+            self.min_left,
+            self.max_left,
+            self.min_right,
+            self.max_right,
+        ) = self.left_right_split(mu, remaining_points, distances)
+
+        ## Build the left and right trees if needed
+        if points_left is not None:
+            self.left = MetricTree().build(points_left)
+        if points_right is not None:
+            self.right = MetricTree().build(points_right)
         return self
